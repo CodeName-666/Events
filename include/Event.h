fb2a25e7892060d8d7f0642f9e790bb09d88e692
SeidC <SeidelChristof@googlemail.com>
2022-06-19 00:41:50 +0200

    Simplify the Event_Connect Macros.

--- include/Event.h
+++ include/Event.h
@@ -5,7 +5,7 @@
 #include "EventSlot.h"
 
 
-#define EVENT_SIGNAL(__name__,args...)                               \
+#define EVENT_SIGNAL(__name__,args...)                                              \
     EventSignal<args> m_##__name__##_signal
 
 /**
@@ -13,40 +13,26 @@
  * @param[in] __name__ Name of the Slot
  * @param[in] __type__ Type of the Slot
  */
-#define EVENT_SLOT(__name__,args...)                                                 \
+#define EVENT_SLOT(__name__,args...)                                               \
        EventSlot<args> m_##__name__##_slot{this , &ThisEventClass::__name__##Slot} \
 
 /**
  * @brief Global Macro defintion for classes to register for slots
  */
-#define EVENT_REG_FOR_SLOTS(__CLASS_NAME__)                                 \
+#define EVENT_REG_FOR_SLOTS(__CLASS_NAME__)                                       \
         template <class... T> using EventSlot = MethodSlot<__CLASS_NAME__, T...>; \
         using ThisEventClass = __CLASS_NAME__;
 
 /**
- * @brief Global Macro definiton of Event Signal 2 Signal connector.
- */
-#define EVENT_CONNECT_SIGNAL2SIGNAL(SIG_SOURCE,SIG_DEST)        \
-    do                                                          \
-    {                                                           \
-        if((&SIG_SOURCE != nullptr) && (&SIG_DEST != nullptr))  \
-        {                                                       \
-            SIG_SOURCE.connect(SIG_DEST);                       \
-        }                                                       \
-                                                                \
-    }while(0)                                                       
-
-
-/**
  * @brief Global Macro definiton of Event Signal 2 Slot connector
  */
-#define EVENT_CONNECT_SIGNAL2SLOT(SIGNAL,SLOT)           \
-    do                                                   \
-    {                                                    \
-        if((&SIGNAL != nullptr) && (&SLOT != nullptr))   \
-        {                                                \
-            SIGNAL.connect(SLOT);                        \
-        }                                                \
+#define EVENT_CONNECT(SIGNAL,SLOT_OR_SIGNAL)                       \
+    do                                                             \
+    {                                                              \
+        if((&SIGNAL != nullptr) && (&SLOT_OR_SIGNAL != nullptr))   \
+        {                                                          \
+            SIGNAL.connect(SLOT_OR_SIGNAL);                                  \
+        }                                                          \
     }while(0)    
 
 
@@ -54,12 +40,12 @@
  * @brief Global Macro definiton of Event Signal emitter
  */
 #define EVENT_EMIT(SIGNAL,args...)  \
-    do                            \
-    {                             \
-        if((&SIGNAL != nullptr))  \
-        {                         \
-            SIGNAL.emit(args);   \
-        }                         \
+    do                              \
+    {                               \
+        if((&SIGNAL != nullptr))    \
+        {                           \
+            SIGNAL.emit(args);      \
+        }                           \
     }while(0) 
 
 /**
